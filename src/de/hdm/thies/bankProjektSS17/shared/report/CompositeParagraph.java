a620bb0dd267632ca8674b45d7b8155acdf6e844
Fabian <fk073@hdm-stuttgart.de>
2017-04-22 14:53:22 +0200

    Methoden und Dokumentation in der Klasse CompositeParagraph und SimpleParagraph eingefügt.

--- src/de/hdm/thies/bankProjektSS17/shared/report/CompositeParagraph.java
+++ src/de/hdm/thies/bankProjektSS17/shared/report/CompositeParagraph.java
@@ -1,5 +1,85 @@
 package de.hdm.thies.bankProjektSS17.shared.report;
 
-public class CompositeParagraph {
+import java.util.Vector;
 
+public class CompositeParagraph extends Paragraph{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * In diesem Vector werden Unterabschnitte gespeichert.
+	 */
+	private Vector<SimpleParagraph> subParagraphs = new Vector<SimpleParagraph>();
+	
+	
+	/**
+	 * Hinzufügen eines Unterabschnittes.
+	 * @param p der hinzuzufügende Unterabschnitt.
+	 */
+	public void addSubParagraph(SimpleParagraph p) {
+		this.subParagraphs.addElement(p);
+	}
+	  
+	/**
+	 * Entfernen eines Unterabschnittes.
+	 * @param p der zu entfernende Unterabschnitt.
+	 */
+	public void removeSubParagraph(SimpleParagraph p) {
+		this.subParagraphs.removeElement(p);
+	}
+	
+	/**
+	 * Auslesen der Unterabschnitte.
+	 * @return Vector-Objekt, der sämtliche Unterabschnitte enthält.
+	 */
+	public Vector<SimpleParagraph> getSubParagraphs() {
+		return this.subParagraphs;
+	}
+	  
+	/**
+	 * Auslesen der Anzahl von Unterabschnitten.
+	 * @return int die Anzahl der Unterabschnitte.
+	 */
+	public int getParagraphsSize() {
+		return this.subParagraphs.size();
+	}
+	
+	/**
+	 * Auslesen eines bestimmten Unterabschnitts.
+	 * @param i Index des auszulesenden Unterabschnitt.
+	 * @return den gewünschten Unterabschnitt.
+	 */
+	public SimpleParagraph getParagraphByIndex(int i) {
+		return this.subParagraphs.elementAt(i);
+	}
+	
+	/**
+	 * Umwandeln eines CompositeParagraphs in einen String.
+	 */
+	public String toString() {
+	    /*
+	     * Wir legen einen leeren Buffer an, in den wir sukzessive sämtliche
+	     * String-Repräsentationen der Unterabschnitte eintragen.
+	     */
+	    StringBuffer result = new StringBuffer();
+
+	    // Schleife über alle Unterabschnitte
+	    for (int i = 0; i < this.subParagraphs.size(); i++) {
+	      SimpleParagraph p = this.subParagraphs.elementAt(i);
+
+	      /*
+	       * den jew. Unterabschnitt in einen String wandeln und an den Buffer hängen.
+	       */
+	      result.append(p.toString() + "\n");
+	    }
+
+	    /*
+	     * Schließlich wird der Buffer in einen String umgewandelt und
+	     * zurückgegeben.
+	     */
+	    return result.toString();
+	  }
 }
--- src/de/hdm/thies/bankProjektSS17/shared/report/SimpleParagraph.java
+++ src/de/hdm/thies/bankProjektSS17/shared/report/SimpleParagraph.java
@@ -1,5 +1,60 @@
 package de.hdm.thies.bankProjektSS17.shared.report;
 
-public class SimpleParagraph {
 
+/**
+ * TODO
+ * @author Fabian
+ *
+ */
+public class SimpleParagraph extends Paragraph{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	private String text = "";
+
+	/**
+	 * TODO
+	 */
+	public SimpleParagraph(){
+	}
+	
+	/**
+	 * Durch diesen Konstruktor kann bereits bei der Insantiierung eines
+	 * SimpleParagraph Objektes der zugehörige Inhalt gesetzt werden.
+	 * @param t Inhalt des Absatzes.
+	 */
+	public SimpleParagraph(String t){
+		this.text = t;
+	}
+	
+	/**
+	 * Auslesen des Inhaltes eines Absatzes.
+	 * @return Inhalt als String
+	 */
+	public String getText() {
+		return text;
+	}
+
+	/**
+	 * Setzen des Inhaltes eines Absatzes.
+	 * @param text der Inhalt des Absatzes.
+	 */
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	
+	/**
+	 * Umwandeln des Simple-Paragrapg Objekts in einen String. 
+	 */
+	public String toString() {
+		return this.text;
+	}
+	
+	
+	
 }
