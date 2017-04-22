fba471e445505f34c4997338d6077bf318e16ede
Fabian <fk073@hdm-stuttgart.de>
2017-04-22 13:58:05 +0200

    Benötigte Methoden in der Klasse Row hinzugefügt und dokumentiert.

--- src/de/hdm/thies/bankProjektSS17/shared/report/Row.java
+++ src/de/hdm/thies/bankProjektSS17/shared/report/Row.java
@@ -1,5 +1,57 @@
 package de.hdm.thies.bankProjektSS17.shared.report;
 
-public class Row {
+import java.io.Serializable;
+import java.util.Vector;
 
+public class Row implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Vector<Column> columns = new Vector<Column>();
+	
+	
+	/**
+	 * Hinzufügen einer Spalte.
+	 * @param c
+	 */
+	
+	public void addColum(Column c){
+		this.columns.addElement(c);
+	}
+	
+	/**
+	 * Löschen einer Spalte.
+	 * @param c
+	 */
+	public void removeColumn(Column c){
+		this.columns.removeElement(c);
+	}
+	
+	/**
+	 * Auslesen sämtlicher Spalten.
+	 * @return ein Vector-Obejekt mit sämtlichen Spalten
+	 */
+	public Vector<Column> getColumns(){
+		return this.columns;
+	}
+	
+	/**
+	 * Auslesen der Anzahl sämtlicher Spalten.
+	 * @return int Anzahl der Spalten
+	 */
+	public int getColumnsSize(){
+		return this.columns.size();
+	}
+	
+	/**
+	 * Auslesen eines einzelnen Spalten-Objekts.
+	 * @param i der Index der auszulesenden Spalte.
+	 * @return das gewünschte Spaltenobjekt.
+	 */
+	public Column getColumnByIndex(int i){
+		return this.columns.elementAt(i);
+	}
 }
