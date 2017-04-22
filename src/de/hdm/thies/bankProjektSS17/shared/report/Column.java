14de38c2a620584f7571b6d9f589ab7c7740f160
Fabian <fk073@hdm-stuttgart.de>
2017-04-22 13:47:08 +0200

    Getter und Setter der Klasse Column verfasst.

--- src/de/hdm/thies/bankProjektSS17/shared/report/Column.java
+++ src/de/hdm/thies/bankProjektSS17/shared/report/Column.java
@@ -1,5 +1,35 @@
 package de.hdm.thies.bankProjektSS17.shared.report;
 
-public class Column {
+import java.io.Serializable;
+
+public class Column implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String value = "";
+
+	
+	public Column(){	
+	}
+	
+	public Column(String v){
+		this.value = v;
+	}
+	
+	
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	public String toString(){
+		return this.value;
+	}
 
 }
