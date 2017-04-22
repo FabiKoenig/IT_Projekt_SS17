fa74bc5f55dd030ffa959b8e1eebe49ba83e3fb9
Fabian <fk073@hdm-stuttgart.de>
2017-04-22 16:27:13 +0200

    Variablen und Methoden der Klasse ServersideSettings angelegt.

--- src/de/hdm/itProjektSS17/server/ServersideSettings.java
+++ src/de/hdm/itProjektSS17/server/ServersideSettings.java
@@ -1,5 +1,15 @@
 package de.hdm.itProjektSS17.server;
 
-public class ServersideSettings {
+import java.util.logging.Logger;
 
+import de.hdm.itProjektSS17.shared.CommonSettings;
+
+public class ServersideSettings extends CommonSettings{
+
+	private static final String LOGGER_NAME = "BankProjekt Server";
+	private static final Logger log = Logger.getLogger(LOGGER_NAME);
+	
+	public static Logger getLogger(){
+		return log;
+	}
 }
