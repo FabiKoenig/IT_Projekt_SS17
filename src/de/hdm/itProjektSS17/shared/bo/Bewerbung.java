cbf0edc0f96d59c561a79222ffbce1d59c0bc88a
ChristianToth <christian@localhost>
2017-04-22 14:03:05 +0200

    Kommentare angelegt für die Klassen Bewerbung und Projekt.

--- src/de/hdm/itProjektSS17/shared/bo/Bewerbung.java
+++ src/de/hdm/itProjektSS17/shared/bo/Bewerbung.java
@@ -3,19 +3,42 @@ package de.hdm.itProjektSS17.shared.bo;
 import java.util.Date;
 
 public class Bewerbung {
-
+	
+	/**
+	   * Erstellungsdatum der Bewerbung
+	   */
 	private  Date erstellungsdatum;
+	
+	/**
+	   * Bewerbungstext der Bewerbung
+	   */
 	private String bewerbungstext;
 	
+	
+	/**
+	   * Auslesen des Erstellungsdatum
+	   */
 	public Date getErstellungsdatum() {
 		return erstellungsdatum;
 	}
+	
+	/**
+	   * Setzen des Erstellungsdatum
+	   */
 	public void setErstellungsdatum(Date erstellungsdatum) {
 		this.erstellungsdatum = erstellungsdatum;
 	}
+	
+	/**
+	   * Auslesen des Bewerbungstext
+	   */
 	public String getBewerbungstext() {
 		return bewerbungstext;
 	}
+	
+	/**
+	   * Setzen des Bewerbungstext
+	   */
 	public void setBewerbungstext(String bewerbungstext) {
 		this.bewerbungstext = bewerbungstext;
 	}
--- src/de/hdm/itProjektSS17/shared/bo/Projekt.java
+++ src/de/hdm/itProjektSS17/shared/bo/Projekt.java
@@ -3,34 +3,79 @@ package de.hdm.itProjektSS17.shared.bo;
 import java.sql.Date;
 
 public class Projekt {
-
+	
+	/**
+	   * Startdatum des Projekt.
+	   */
 	private Date startdatum; 
+	
+	/**
+	   * Enddatum des Projekt
+	   */
 	private Date enddatum;
+	
+	/**
+	   * Name des Projekt
+	   */
 	private String name;
+	
+	/**
+	   * Beschreibung des Projekt
+	   */
 	private String beschreibung;
 	
-	
+	 /**
+	   * Auslesen des Startdatum.
+	   */
 	public Date getStartdatum(){
 		return startdatum;
 	}
+	
+	/**
+	   * Setzen des Startdatum.
+	   */
 	public void setStartdatum(Date startdatum){
 		this.startdatum = startdatum;
 	}
+	
+	 /**
+	   * Auslesen des Enddatum.
+	   */
 	public Date getEnddatum() {
 		return enddatum;
 	}
+	
+	/**
+	   * Setzen des Enddatum.
+	   */
 	public void setEnddatum(Date enddatum) {
 		this.enddatum = enddatum;
 	}
+	
+	 /**
+	   * Auslesen des Namen.
+	   */
 	public String getName() {
 		return name;
 	}
+	
+	/**
+	   * Setzen des Namen.
+	   */
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	/**
+	   * Auslesen der Beschreibung.
+	   */
 	public String getBeschreibung() {
 		return beschreibung;
 	}
+	
+	/**
+	   * Setzen der Beschreibung.
+	   */
 	public void setBeschreibung(String beschreibung) {
 		this.beschreibung = beschreibung;
 	}
