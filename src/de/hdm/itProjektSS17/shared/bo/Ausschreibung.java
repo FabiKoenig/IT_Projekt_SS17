4d4f4b6a8428a89887ada425aeffa987668434d3
Fabian <fk073@hdm-stuttgart.de>
2017-04-22 12:49:29 +0200

    Klasse Ausschreibung angelegt und variablen deklariert.

---
+++ src/de/hdm/itProjektSS17/shared/bo/Ausschreibung.java
@@ -0,0 +1,16 @@
+package de.hdm.itProjektSS17.shared.bo;
+
+import java.util.Date;
+
+public class Ausschreibung extends BusinessObject{
+
+	private String bezeichung = "";
+	
+	private Date bewerbungsfrist = null;
+	
+	private String ausschreibungstext = "";
+	
+	
+	
+	
+}
