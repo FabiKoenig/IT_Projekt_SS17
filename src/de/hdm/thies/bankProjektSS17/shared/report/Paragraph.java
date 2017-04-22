fed8c3f21d79f39ba87257432efe2a1c128a3ab4
Fabian <fk073@hdm-stuttgart.de>
2017-04-22 14:32:59 +0200

    Klasse Paragraph implementiert Serializable.

--- src/de/hdm/thies/bankProjektSS17/shared/report/Paragraph.java
+++ src/de/hdm/thies/bankProjektSS17/shared/report/Paragraph.java
@@ -1,5 +1,13 @@
 package de.hdm.thies.bankProjektSS17.shared.report;
 
-public class Paragraph {
+import java.io.Serializable;
 
+public class Paragraph implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
 }
