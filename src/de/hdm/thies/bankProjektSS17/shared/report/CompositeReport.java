dc5a0779d6ac7720ae9196a475efca9460788f99
Fabian <fk073@hdm-stuttgart.de>
2017-04-22 14:25:03 +0200

    Methoden der Klasse CompositeReport hinzugefügt und kommentiert.

--- src/de/hdm/thies/bankProjektSS17/shared/report/CompositeReport.java
+++ src/de/hdm/thies/bankProjektSS17/shared/report/CompositeReport.java
@@ -1,5 +1,56 @@
 package de.hdm.thies.bankProjektSS17.shared.report;
 
-public class CompositeReport {
+import java.util.Vector;
 
+/**
+ * (TODO)
+ * @author Fabian
+ *
+ */
+
+public class CompositeReport extends Report{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * In dem Vector-Obejekt werden die Teil-Reports gespeichert.
+	 */
+	private Vector<Report> subReports = new Vector<Report>();
+	
+	
+	/**
+	 * Hinzufügen eines Teil-Reports.
+	 * @param r der hinzuzufügende Teil-Report.
+	 */
+	public void addSubReport(Report r){
+		this.subReports.addElement(r);
+	}
+	
+	/**
+	 * Löschen eines Teil-Reports.
+	 * @param r der zu löschende Report.
+	 */
+	public void removeSubReport(Report r) {
+		this.subReports.removeElement(r);
+	}
+	
+	/**
+	 * Auslesen der Anzahl von Teil-Reports.
+	 * @return
+	 */
+	public int getSubReportsSize() {
+		return this.subReports.size();
+	}
+	
+	/**
+	 * Auslesen eines bestimmten Teil-Reports.
+	 * @param i Index des auszulesenden Teil-Reports.
+	 * @return das Report Objekt des auszulesenden Teil-Reports.
+	 */
+	public Report getSubReportByIndex(int i) {
+		return this.subReports.elementAt(i);
+	}
 }
