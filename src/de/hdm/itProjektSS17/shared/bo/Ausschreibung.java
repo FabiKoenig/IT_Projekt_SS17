2e907769d3e9382f5865b412d1e50a258c6f481e
Fabian <fk073@hdm-stuttgart.de>
2017-04-22 13:00:42 +0200

    Getter und Setter in der Klasse Ausschreibung angelegt. Klasse Bewertung erstellt, Variablen deklariert und Getter und Setter generiert.

--- src/de/hdm/itProjektSS17/shared/bo/Ausschreibung.java
+++ src/de/hdm/itProjektSS17/shared/bo/Ausschreibung.java
@@ -4,11 +4,43 @@ import java.util.Date;
 
 public class Ausschreibung extends BusinessObject{
 
+
+	private static final long serialVersionUID = 1L;
+
 	private String bezeichung = "";
 	
 	private Date bewerbungsfrist = null;
 	
 	private String ausschreibungstext = "";
+	
+	
+	
+	
+	
+	
+	public String getAusschreibungstext() {
+		return ausschreibungstext;
+	}
+
+	public String getBezeichung() {
+		return bezeichung;
+	}
+
+	public void setBezeichung(String bezeichung) {
+		this.bezeichung = bezeichung;
+	}
+
+	public Date getBewerbungsfrist() {
+		return bewerbungsfrist;
+	}
+
+	public void setBewerbungsfrist(Date bewerbungsfrist) {
+		this.bewerbungsfrist = bewerbungsfrist;
+	}
+
+	public void setAusschreibungstext(String ausschreibungstext) {
+		this.ausschreibungstext = ausschreibungstext;
+	}
 	
 	
 	
