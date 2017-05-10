8d99908da1434234e9b292eb2ab795d1ddb054f3
TimGuell <tg070@hdm-stuttgart.de>
2017-05-10 17:27:08 +0200

    <Person> findByForeignTeamId um If Abfrage erweitert

--- src/de/hdm/itProjektSS17/server/db/PersonMapper.java
+++ src/de/hdm/itProjektSS17/server/db/PersonMapper.java
@@ -124,12 +124,17 @@ public class PersonMapper extends OrganisationseinheitMapper{
 			
 				result.add(p);
 				} 
+				if(result.isEmpty()==true){
+	        	  return null;
+					}else{
+	              return result;
+	          }
 			}   
 		catch (SQLException e) {
 		e.printStackTrace();
+		return null;
 		}
 		
-		return result;
 	}
 	/**
 	 * 
