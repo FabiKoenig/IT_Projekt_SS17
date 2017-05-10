eb31bb9dfebd32be6108c78024c6594637e6b81b
Michael Geiselmann <mg155@hdm-stuttgart.de>
2017-05-10 12:52:36 +0200

    Partnerprofil_Id in der Methode createUnternehmen und Insert-Methode im Mapper angepasst.

--- src/de/hdm/itProjektSS17/server/ProjektmarktplatzVerwaltungImpl.java
+++ src/de/hdm/itProjektSS17/server/ProjektmarktplatzVerwaltungImpl.java
@@ -359,14 +359,13 @@ implements ProjektmarktplatzVerwaltung {
 	 */
 	@Override
 
-	public Unternehmen createUnternehmen(String name, String hausnummer, String ort, int plz, String strasse, int partnerprofilId) throws IllegalArgumentException {
+	public Unternehmen createUnternehmen(String name, String hausnummer, String ort, int plz, String strasse) throws IllegalArgumentException {
 		Unternehmen u = new Unternehmen();
 		u.setName(name);
 		u.setHausnummer(hausnummer);
 		u.setOrt(ort);
 		u.setPlz(plz);
 		u.setStrasse(strasse);
-		u.setPartnerprofilId(partnerprofilId);
 		/*
 	     * Setzen einer vorläufigen OrganisationsId. Der insert-Aufruf liefert dann ein
 	     * Objekt, dessen Nummer mit der Datenbank konsistent ist.
