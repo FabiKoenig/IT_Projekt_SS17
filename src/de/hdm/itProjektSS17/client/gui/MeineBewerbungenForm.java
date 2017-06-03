d6053bccf18bbb4e72b7edd0a38ebc6183685492
TomAlender <ta028@hdm-stuttgart.de>
2017-06-03 13:56:27 +0200

    bugfixes

--- src/de/hdm/itProjektSS17/client/gui/MeineBewerbungenForm.java
+++ src/de/hdm/itProjektSS17/client/gui/MeineBewerbungenForm.java
@@ -352,7 +352,9 @@ public class MeineBewerbungenForm extends Showcase{
 					public void onSuccess(Void result) {
 
 						Window.alert("Das Zurückziehen der Bewerbung war erfolgreich!");
-						Navigation.reload();
+						Showcase showcase = new MeineBewerbungenForm();
+						RootPanel.get("Details").clear();
+						RootPanel.get("Details").add(showcase);
 					;
 						
 					}
