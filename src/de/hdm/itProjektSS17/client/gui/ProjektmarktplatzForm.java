33e5cce7d23ea4813ce934df95c66d5a255eae71
Michael Geiselmann <mg155@hdm-stuttgart.de>
2017-06-14 11:15:28 +0200

    Pager Problem bei Projektmarktplatz gefixt

--- src/de/hdm/itProjektSS17/client/gui/ProjektmarktplatzForm.java
+++ src/de/hdm/itProjektSS17/client/gui/ProjektmarktplatzForm.java
@@ -243,6 +243,23 @@ public class ProjektmarktplatzForm extends Showcase {
 							
 							ct_eigeneProjektmarktplaetze.setRowData(0, result);
 							ct_eigeneProjektmarktplaetze.setRowCount(result.size(), true);
+							
+							
+							final ListDataProvider dataProvider1 = new ListDataProvider();
+							SimplePager pager1;
+							SimplePager.Resources pagerResources1 = GWT.create(SimplePager.Resources.class);
+							pager1 = new SimplePager(TextLocation.CENTER, pagerResources1, false, 0, true);
+							pager1.setDisplay(ct_eigeneProjektmarktplaetze);
+							dataProvider1.addDataDisplay(ct_eigeneProjektmarktplaetze);
+							dataProvider1.setList(new ArrayList<Projektmarktplatz>(beigetreteneProjektmarktplaetze));
+							pager1.setPageSize(5);
+							
+							HorizontalPanel hp_pager1 = new HorizontalPanel();
+							hp_pager1.setWidth("100%");
+							hp_pager1.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+							hp_pager1.add(pager1);
+							add(hp_pager1);
+							
 							}
 
 						
@@ -318,31 +335,8 @@ public class ProjektmarktplatzForm extends Showcase {
 		        selectedObject_eigeneProjektmarktplaetze = ssm_eigeneProjektmarktplaetze.getSelectedObject();
 		    }
 		});
-		
-		
-		
 		ct_eigeneProjektmarktplaetze.setWidth("100%");
 		
 		this.add(ct_eigeneProjektmarktplaetze);
-		
-		final ListDataProvider dataProvider1 = new ListDataProvider();
-		SimplePager pager1;
-		SimplePager.Resources pagerResources1 = GWT.create(SimplePager.Resources.class);
-		pager1 = new SimplePager(TextLocation.CENTER, pagerResources1, false, 0, true);
-		pager1.setDisplay(ct_eigeneProjektmarktplaetze);
-		dataProvider1.addDataDisplay(ct_eigeneProjektmarktplaetze);
-		dataProvider1.setList(new ArrayList<Projektmarktplatz>(beigetreteneProjektmarktplaetze));
-		pager1.setPageSize(5);
-		
-		HorizontalPanel hp_pager1 = new HorizontalPanel();
-		hp_pager1.setWidth("100%");
-		hp_pager1.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
-		hp_pager1.add(pager1);
-		add(hp_pager1);
-		
 		}		
-		
-	
 	}
-
-
